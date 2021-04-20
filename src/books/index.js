6b33149b3ff8f91ad955b3118f3a7e1a986c3d8c
Can Soylu <hsyncn17@gmail.com>
2021-04-20 17:38:00 +0300

    initial commit

--- src/books/index.js
+++ src/books/index.js
@@ -1,61 +1,107 @@
-import express from "express"
-import fs from "fs-extra"
-import { fileURLToPath } from "url"
-import { dirname, join } from "path"
-import multer from "multer"
-import { CloudinaryStorage } from "multer-storage-cloudinary"
-import { v2 } from "cloudinary"
+import express from "express";
+import fs, { writeJSON } from "fs-extra";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 } from "cloudinary";
+import uniqid from "uniqid";
 
-const dataFolder = join(dirname(fileURLToPath(import.meta.url)), "../data")
-const booksPath = join(dataFolder, "books.json")
+const dataFolder = join(dirname(fileURLToPath(import.meta.url)), "../data");
+const booksPath = join(dataFolder, "books.json");
+const commentsPath = join(dataFolder, "comments.json");
 
 const cloudinaryStorage = new CloudinaryStorage({
   cloudinary: v2,
   params: {
     folder: "strive",
   },
-})
+});
 
-const uploader = multer({ storage: cloudinaryStorage })
+const uploader = multer({ storage: cloudinaryStorage });
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const books = await fs.readJSON(booksPath)
-    res.send(books)
+    const books = await fs.readJSON(booksPath);
+    res.send(books);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 router.get("/:asin", async (req, res, next) => {
   try {
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 router.post("/", uploader.single("cover"), async (req, res, next) => {
   try {
-    res.send({ cloudinaryURL: req.file.path })
+    res.send({ cloudinaryURL: req.file.path });
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
+
+router.post("/:asin/comments", async (req, res, next) => {
+  try {
+    const books = await fs.readJSON(booksPath);
+    // const comments = await fs.readJSON(commentsPath);
+
+    const moddedBooksArray = books.filter(
+      (book) => book.asin !== req.params.asin
+    );
+
+    let selectedBook = books.find(book.asin === req.params.asin);
+
+    if (!selectedBook) {
+      err = new Error({ errMsg: "book not found" });
+      err.httpStatusCode = 404;
+      next(err);
+    }
+    const newComment = { ...req.body, _id: uniqid(), date: new Date() };
+    const moddedBook = {
+      ...selectedBook,
+      comments: [...selectedBook.comments, newComment],
+    };
+
+    moddedBooksArray.push(moddedBook);
+    await fs.writeJSON(booksPath, moddedBooksArray);
+
+    res
+      .status(201)
+      .send({ msg: "Comment successfully updated!", data: { newComment } });
+
+    res.send();
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:asin/comments", async (req, res, next) => {
+  try {
+    const comments = await fs.readJSON(commentsPath);
+    const selectedComments = comments;
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.put("/:asin", async (req, res, next) => {
   try {
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 router.delete("/:asin", async (req, res, next) => {
   try {
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-export default router
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,33 +1,33 @@
-import express from "express"
-import cors from "cors"
-import listEndpoints from "express-list-endpoints"
+import express from "express";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
 
-import booksRoutes from "./books/index.js"
+import booksRoutes from "./books/index.js";
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT // no need to configure it manually on Heroku
+const port = process.env.PORT || 5000; // no need to configure it manually on Heroku
 
 //app.use(cors()) // no options means allow everybody
 
 // app.use(cors({ origin: process.env.FE_URL })) // "http://localhost:3000" is the only origin allowed
 
-const whitelist = [process.env.FE_URL_DEV, process.env.FE_URL_PROD] // You NEED to configure it manually on Heroku
+const whitelist = [process.env.FE_URL_DEV, process.env.FE_URL_PROD]; // You NEED to configure it manually on Heroku
 
 const corsOptions = {
   origin: function (origin, next) {
     if (whitelist.indexOf(origin) !== -1) {
-      console.log("ORIGIN ", origin)
+      console.log("ORIGIN ", origin);
       // origin found in whitelist
-      next(null, true)
+      next(null, true);
     } else {
       // origin not found in the whitelist
-      next(new Error("Not allowed by CORS"))
+      next(new Error("Not allowed by CORS"));
     }
   },
-}
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 // CROSS ORIGIN RESOURCE SHARING
 
@@ -47,15 +47,15 @@ app.use(cors(corsOptions))
 
 // routers
 
-app.use("/books", booksRoutes)
+app.use("/books", booksRoutes);
 
-console.log(listEndpoints(app))
+console.log(listEndpoints(app));
 
 app.listen(port, () => {
   if (process.env.NODE_ENV === "production") {
     // no need to configure it manually on Heroku
-    console.log("Server running on cloud on port: ", port)
+    console.log("Server running on cloud on port: ", port);
   } else {
-    console.log("Server running locally on port: ", port)
+    console.log("Server running locally on port: ", port);
   }
-})
+});
